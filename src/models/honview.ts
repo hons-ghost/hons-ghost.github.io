479a0d8fb19db5e07e81f1605200b6ba83055d1d
zcbm4321 <zcbm4321@gmail.com>
2023-12-02 11:57:57 +0900

    design change

--- src/models/honview.ts
+++ src/models/honview.ts
@@ -4,18 +4,24 @@ import { elapsedTime} from "../utils.js";
 export const DrawHtmlHonItem = (uniqId: string, nickname: string, 
     email: string, content: string, time: number): string => {
     return `
-        <br>
-            <div class="card">
-                <div class="card-header"> 
+<div class="container p-2 border-top">
+    <div class="row">
+        <div class="col-1 text-center">
                     <span id="${uniqId}" class="m-1"></span>
+        </div>
+        <div class="col">
+            <div class="container">
+                <div class="row">
                     <a href="javascript:void(0)" onclick="ClickLoadPage('hondetail', false, '&email=${email}')">
                     <strong class="me-auto">${nickname}</strong>
                     </a>
-                    <small> ${elapsedTime(Number(time))}</small>
+                    <small>@${email} · ${elapsedTime(Number(time))}</small>
                 </div>
-                <div class="card-body">
-                    ${content}
+                <div class="row">
+                    <pre>${content}</pre>
                 </div>
-            </div>
+        </div>
+    </div>
+</div>
         `;
 }

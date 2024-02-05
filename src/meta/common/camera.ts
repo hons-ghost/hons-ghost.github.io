12be52a3d0a0617d1abffe763a1942a14278f8da
zcbm4321 <zcbm4321@gmail.com>
2024-02-05 14:58:35 +0900

    bug fix

--- src/meta/common/camera.ts
+++ src/meta/common/camera.ts
@@ -8,13 +8,15 @@ import { Gui } from "../factory/appfactory"
 
 export class Camera extends THREE.PerspectiveCamera implements IViewer{
     controls?: OrbitControls
-    movingPos: THREE.Vector3
+    longPos: THREE.Vector3
+    shortPos: THREE.Vector3
     viewMode: string
     constructor(canvas: Canvas, private player: IPhysicsObject) {
         super(75, canvas.Width/ canvas.Height, 0.1, 500)
         canvas.RegisterViewer(this)
         this.controls = new OrbitControls(this, canvas.Canvas)
-        this.movingPos = new THREE.Vector3(0, 44, 79)
+        this.longPos = new THREE.Vector3(0, 44, 79)
+        this.shortPos = new THREE.Vector3(0, 0, 0)
         this.viewMode = ""
         Gui.add(this.position, 'x', 0, 100, 1).listen()
         Gui.add(this.position, 'y', 0, 100, 1).listen()
@@ -32,10 +34,10 @@ export class Camera extends THREE.PerspectiveCamera implements IViewer{
         const position = this.player.Position
         this.rotation.x = -Math.PI / 4
         this.position.set(position.x, position.y, position.z)
-        gsap.to(this.movingPos, { x: 0, y: 17, z: 33, 
-            duration: 5, ease: "power1.inOut", onUpdate: () => {
-                this.position.set(this.movingPos.x, this.movingPos.y,
-                    this.movingPos.z)
+        gsap.to(this.longPos, { x: 0, y: 17, z: 33, 
+            duration: 4, ease: "power1.inOut", onUpdate: () => {
+                this.position.set(this.longPos.x, this.longPos.y,
+                    this.longPos.z)
             }})
     }
 
@@ -44,30 +46,23 @@ export class Camera extends THREE.PerspectiveCamera implements IViewer{
         this.player.Visible = true
         this.rotation.x = -Math.PI / 4
         this.position.set(0, 16, 15)
-        this.movingPos.set(0, 13, 13)
+        this.shortPos.set(0, 13, 13)
 
     }
 
     update() {
-        if (this.viewMode == "long") {
-            this.controls?.update()
-            return
-        }
         const position = this.player.Position
-        const mode = "far"
-        switch (mode) {
-            /*
-            case "near":
-                this.rotation.x = -0.6
-                this.position.set(position.x, position.y + 2, position.z + 2.3)
+        switch (this.viewMode) {
+            case "long":
+                this.controls?.update()
                 break;
-                */
-            case "far":
-                //this.lookAt(position.x, position.y, position.z)
+            case "close":
+                this.lookAt(position.x, position.y, position.z)
                 this.rotation.x = -Math.PI / 4
-                this.position.set(position.x + this.movingPos.x, 
-                    position.y + this.movingPos.y, 
-                    position.z + this.movingPos.z)
+                this.position.set(
+                    position.x + this.shortPos.x, 
+                    position.y + this.shortPos.y, 
+                    position.z + this.shortPos.z)
                 break;
         }
     }
